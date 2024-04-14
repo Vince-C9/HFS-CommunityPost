f347253496d6d721aa63c664de530fb243d7f009
vince <vincecole6@hotmail.co.uk>
2024-04-14 22:42:21 +0100

    Set up factory

---
+++ api/database/factories/CommentFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Comment>
+ */
+class CommentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'comment' => fake()->sentence(),
+            'user_id' => fake()->randomNumber(),
+            'parent_id' => fake()->randomNumber(),
+            'article_id' => fake()->randomNumber(),
+        ];
+    }
+}
