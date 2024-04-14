c525ef1d3e159e704846303ebd3f48dba71f0f64
vince <vincecole6@hotmail.co.uk>
2024-04-14 22:18:00 +0100

    Article Controller

--- api/app/Http/Controllers/ArticleController.php
+++ api/app/Http/Controllers/ArticleController.php
@@ -2,33 +2,57 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Article\DestroyArticleRequest;
+use App\Http\Requests\Article\StoreArticleRequest;
+use App\Http\Requests\Article\UpdateArticleRequest;
 use App\Models\Article;
 use Illuminate\Http\Request;
+use Throwable;
 
 class ArticleController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
-    }
+        $articles = Article::get();
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        return response()->json([
+            'status' => 'article.get',
+            'articles' => !empty($articles) ? $articles : [],
+            'message' => 'Fetched article list.',
+        ], 200);
+
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreArticleRequest $request)
     {
-        //
+        try{
+            $article = Article::create([
+                'title' => $request->input('title'),
+                'content' => $request->input('content'),
+                'user_id' => auth('sanctum')->user()->id
+            ]);
+
+            return response()->json([
+                'status' => 'article.get',
+                'article' => $article->id,
+                'message' => 'Article saved.',
+            ], 200);
+        }
+        catch(Throwable $t){
+            
+            dd($t->getMessage());
+            return response()->json([
+                'status' => $t->getCode(),
+                'message' => $t->getMessage(),
+            ], 500);
+        }
+        
     }
 
     /**
@@ -36,23 +60,42 @@ class ArticleController extends Controller
      */
     public function show(Article $article)
     {
-        //
-    }
+        try {
+            return response()->json([
+                'status' => 'article.show',
+                'article' => $article,
+                'message' => 'Fetched article.',
+            ], 200); 
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Article $article)
-    {
-        //
+        } catch(Throwable $t) {
+            return response()->json([
+                'status' => $t->getCode(),
+                'message' => $t->getMessage(),
+            ], 500);
+        }
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Article $article)
+    public function update(UpdateArticleRequest $request, Article $article)
     {
-        //
+        try {
+            $article->update([
+                'title' => $request->input('title'),
+                'content' => $request->input('content')
+            ]);
+
+            return response()->json([
+                'status' => 'article.update',
+                'message' => 'Article Updated.',
+            ], 200); 
+        } catch(Throwable $t) {
+            return response()->json([
+                'status' => $t->getCode(),
+                'message' => $t->getMessage(),
+            ], 500);
+        }
     }
 
     /**
@@ -60,6 +103,20 @@ class ArticleController extends Controller
      */
     public function destroy(Article $article)
     {
-        //
+        try {
+            $article->delete();
+
+            return response()->json([
+                'status' => 'article.update',
+                'message' => 'Article deleted.',
+            ], 200); 
+
+        } catch(Throwable $t) {
+            return response()->json([
+                'status' => $t->getCode(),
+                'message' => $t->getMessage(),
+            ], 500);
+        }
+        
     }
 }
